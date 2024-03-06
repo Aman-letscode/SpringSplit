3e9430bacb3b57c840ac6063cfd479e43ba08195
aman <amanmagrawal2002@gmail.com>
2024-03-06 14:59:52 +0530

    First Commit

---
+++ src/main/kotlin/com/springkotlin/springsplit/SpringsplitApplication.kt
@@ -0,0 +1,11 @@
+package com.springkotlin.springsplit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringsplitApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringsplitApplication>(*args)
+}
---
+++ src/test/kotlin/com/springkotlin/springsplit/SpringsplitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springkotlin.springsplit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringsplitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
